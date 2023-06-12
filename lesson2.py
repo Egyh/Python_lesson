07a6423788d4f9e3208164fed960c060dd47ad77
Egyh <124222570+Egyh@users.noreply.github.com>
2023-06-12 07:26:06 +0000

    条件分岐の追加

--- lesson2.py
+++ lesson2.py
@@ -1,17 +1,26 @@
+# 変数 money に数値 1000 を代入してください
+money = 1000
+
 items = {'apple': 100, 'banana': 200, 'orange': 400}
 
 for item_name in items:
     print('--------------------------------------------------')
+    # 変数 money を用いて「 財布には◯◯円入っています 」のように出力してください
+    print('財布には' + str(money) + '円入っています')
+    
     print(item_name + 'は1個' + str(items[item_name]) + '円です')
     
-    # input を用いて入力を受け取り、変数 input_count に代入
-    input_count = input('購入する' + item_name + 'の個数を入力してください:')
-    # キーと変数 input_count を用いて「 購入する◯◯の個数は△△個です 」となるように出力
+    input_count = input('購入する' + item_name + 'の個数を入力してください：')
     print('購入する' + item_name + 'の個数は' + input_count + '個です')
     
-    # input_count を数値として変数 count に代入
-    count  = int(input_count)
-    # 変数 total_price に果物1個の値段と変数 count を掛けた値を代入
-    total_price = items[item_name]*count
-    # 変数 total_price と型変換を用いて、「 支払い金額は◯◯円です 」となるように出力
+    count = int(input_count)
+    total_price = items[item_name] * count
     print('支払い金額は' + str(total_price) + '円です')
+    
+    # money と total_price の比較結果によって条件を分岐してください
+    if money >= total_price:
+      print(item_name+'を'+input_count+'個買いました')
+      money -= total_price
+    else:
+      print('お金が足りません')
+      print(item_name+'を買えませんでした')
